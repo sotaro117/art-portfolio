66dccd4a769d048be3bc18307ad1b95f0e06347e
sotaro117 <82375645+sotaro117@users.noreply.github.com>
2025-03-23 00:22:30 +0100

    fixed env vars

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -14,6 +14,20 @@ export default async function Dashboard() {
   );
 }
 
+const GOOGLE_APPLICATION_CREDENTIALS = {
+  type: process.env.GC_TYPE,
+  project_id: process.env.GC_PROJECT_ID,
+  private_key_id: process.env.GC_PRIVATE_KEY_ID,
+  private_key: process.env.GC_PRIVATE_KEY,
+  client_email: process.env.GC_CLIENT_EMAIL,
+  client_id: process.env.GC_CLIENT_ID,
+  auth_uri: process.env.GC_AUTH_URI,
+  token_uri: process.env.GC_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GC_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GC_CLIENT_CERT_URL,
+  universe_domain: process.env.GC_UNIVERSAL_DOMAIN,
+};
+
 async function runReport() {
   const analyticsDataClient = new BetaAnalyticsDataClient();
   const [response] = await analyticsDataClient.runReport({
